37e259d90c40494fb794758b0ebc2926073fbe1f
Ariel Salem <Ariel.Salem1989@gmail.com>
2018-03-22 12:38:56 -0700

    navbar exists again

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var mongo = require('mongo');
-var mongoose = require('mongoose');
-var Recipe = require('./models');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Recipe = require('./models');
+const app = express();
 
 /*CONNECT TO DATABASE*/
 //make sure to uncomment which connection you're using
 
 var connection = 'mongodb://thyme:thyme@ds133340.mlab.com:33340/orion-thyme';
 // var connection = 'mongodb://localhost/thyme';
-mongoose.connect(connection);
-mongoose.connection.once('open', function() {
+mongoose.connect(connection, (err) => {
+  if(err) {
+    throw 'Error connecting to db - ' + err;
+  }
   console.log('Thyme after thyme on: ' + connection);
 });
 
-
+db = mongoose.connection;
 
 /*CONNECT TO SERVER*/
 var port = process.env.PORT || 3000;
